cc3545d1739e2cfbad1a41ba6bc29f82fbc8ce65
guychannel1 <kkay1334@gmail.com>
2026-07-09 03:06:22 +0700

    feat: integrate vercel web analytics and speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
     import './layout.css';
+    import { onMount } from 'svelte';
+    import { dev } from '$app/environment';
+    import { injectAnalytics } from '@vercel/analytics/sveltekit';
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
     let { children } = $props();
+
+    onMount(() => {
+        injectAnalytics({ mode: dev ? 'development' : 'production' });
+        injectSpeedInsights();
+    });
 </script>
 
 <svelte:head>
     <title>Temp</title>
 </svelte:head>
-
-
 
 <main class="flex-1 w-full z-10 flex flex-col relative">
     {@render children()}
