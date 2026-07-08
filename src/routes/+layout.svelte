6856d46392a840106301825e22bc81a4a4fc5bf8
guychannel1 <kkay1334@gmail.com>
2026-07-08 18:24:04 +0700

    fix: remove glow-bg blobs that broke responsive layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,9 +7,7 @@
     <title>Temp</title>
 </svelte:head>
 
-<!-- Glowing Blobs -->
-<div class="glow-bg top-[-10%] left-[-10%]"></div>
-<div class="glow-bg bottom-[-10%] right-[-10%]"></div>
+
 
 <main class="flex-1 w-full z-10 flex flex-col relative">
     {@render children()}
